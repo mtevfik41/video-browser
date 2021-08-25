c12b2d2a645135d2716dd622c14ccbd7acaaec91
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-25 04:19:09 +0300

    Project finished

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -6,7 +6,8 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResults: 5,
+    type: 'video',
+    maxResults: 10,
     key: KEY,
-  }
-})
+  },
+});
