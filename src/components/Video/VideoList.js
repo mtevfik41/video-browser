6f7c6fff8c6d621f30367a8d1f173a760a443430
Tevfik Kadan <mtevfik41@gmail.com>
2021-08-25 04:18:40 +0300

    Video components

---
+++ src/components/Video/VideoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({videos, onVideoSelect}) => {
+  return (
+      <div className="ui relaxed divided list">
+        {videos.map(video => <VideoItem onVideoSelect={onVideoSelect}
+                                        key={video.id.videoId} video={video}/>)}
+      </div>
+  );
+};
+
+export default VideoList;
